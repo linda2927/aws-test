0ca3d2f1e1118c8458f3c0bf2da3dbf9eb40066c
Jisoo <linda2927@naver.com>
2022-05-19 11:11:18 +0900

    modigy settings for deploy

--- vpc/config/settings/base.py
+++ vpc/config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g-l$*!w3hy6gb(54kz3qtet(*nfqnsb5!vbh^@w)8)3%o(=598
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
 
 
 # Application definition
---
+++ vpc/config/settings/deploy.py
@@ -0,0 +1,3 @@
+from .base import *
+
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
